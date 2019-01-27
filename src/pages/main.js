91dffc1b2814e633cedb7a635b378e2db5384762
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-01-27 17:25:04 -0300

    Get from api

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api from '../services/api';
+import axios from 'axios';
 
 export default class Main extends Component {
     static navigationOptions = {
         title: "JShunt"
     };
+
+    state = {
+        docs: [],
+    };
+
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+
+        this.setState({docs});
+    };
+
     render(){
         return (
             <View>
                 <Text> Página Main </Text>
+                {this.state.docs.map(product => (
+                    <Text key={product._id}>{product.title}</Text>
+                ))}
             </View>
         );
     }
