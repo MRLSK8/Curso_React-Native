7749bead28fe1cf613bed68dd6b94867acb267ea
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-01-27 21:44:28 -0300

    Render info

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 import axios from 'axios';
 
@@ -24,13 +24,25 @@ export default class Main extends Component {
         this.setState({docs});
     };
 
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+
+            <TouchableOpacity onPress={() => {}}>
+                <Text>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
     render(){
         return (
             <View>
-                <Text> Página Main </Text>
-                {this.state.docs.map(product => (
-                    <Text key={product._id}>{product.title}</Text>
-                ))}
+                <FlatList
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         );
     }
